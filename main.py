fa4d40e98a4b7e4c646ce93bc5622599d1fbf5b4
Cecilia <ceciliataboada1@gmail.com>
2023-07-20 21:27:24 -0300

    some changes were done since flask werent rendering the templates

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="..\Controlador-de-gastos-web\templates")
-app.config(debug=True)
+app = Flask(__name__, template_folder="..\Controlador-de-gastos-web\\templates", static_folder="..\Controlador-de-gastos-web\static")
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
