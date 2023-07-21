2fa9ce14ad4a41120ecf78243c4c25cc983bc9af
Cecilia <ceciliataboada1@gmail.com>
2023-07-21 16:40:24 -0300

    database models were fixed

--- models/db_models.py
+++ models/db_models.py
@@ -1,11 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy, func
-from settings.settings_db import app_db
-
-db = SQLAlchemy(app_db) #init database
+from settings.settings_db import db
+from flask_login import UserMixin
 
 """ models for user database """
 
-class User(db.Model):
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150))
     email = db.Column(db.String(50), unique=True) #email must be unique
@@ -15,23 +14,23 @@ class User(db.Model):
 """ models for bills database """
 
 class Rent(db.Model):
+    __tablename__ = "rent"
     id = db.Column(db.Integer, primary_key=True)
     rent = db.Column(db.Integer)
     expenses = db.Column(db.Integer)
     electricity = db.Column(db.Integer)
     gas = db.Column(db.Integer)
     water = db.Column(db.Integer)
-    date = db.Column(db.DateTime(timezone=True), default = func.now()) #takes current time when object is created
+    date = db.Column(db.DateTime(timezone=True)) #takes current time when object is created
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
 """ models for market database """
 
 class Market(db.Model):
+    __tablename__ = "market"
     id = db.Column(db.Integer, primary_key=True)
     market = db.Column(db.Integer)
     market_list = db.Column(db.String(1000)) # <- should be a dictionary with keys and values
     delivery = db.Column(db.Integer)
-    date = db.Column(db.DateTime(timezone=True), default = func.now())
+    date = db.Column(db.DateTime(timezone=True))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-
-db.create_all()
