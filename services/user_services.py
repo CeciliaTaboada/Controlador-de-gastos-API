25ab0957816e41df3f3d7485e4eead92118baab2
Cecilia <ceciliataboada1@gmail.com>
2023-07-22 23:03:07 -0300

    refactoring the code

--- services/user_services.py
+++ services/user_services.py
@@ -2,9 +2,11 @@ from models.db_models import User
 
 class UserServices():
 
+    @staticmethod
     def get_mail(email):
         return User.query.filter_by(email=email).first()
 
+    @staticmethod
     def check_password(password, password1):
         if len(password) >= 8 and password == password1:
             return True
