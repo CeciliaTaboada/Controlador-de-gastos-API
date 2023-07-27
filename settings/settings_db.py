c05623e5b934739b78855bb44a7c09b1fb7770d7
Cecilia <ceciliataboada1@gmail.com>
2023-07-26 21:43:48 -0300

    creating blueprints

--- settings/settings_db.py
+++ settings/settings_db.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from os import environ
 
 db = SQLAlchemy()
@@ -12,9 +13,22 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
+    from views.viewhome import views
+
+    with app.app_context():
+        app.register_blueprint(views, url_prefix="/")
+
     from models.db_models import User, Rent, Market
 
     with app.app_context():
         db.create_all()
+
+    log_man = LoginManager()
+    log_man.login_view = "auth.login"
+    log_man.init_app(app)
     
+    @log_man.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
