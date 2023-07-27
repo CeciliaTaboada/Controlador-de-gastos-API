38a73e5d31c7b3bc233acba0b131d12a91cb29ac
Cecilia <ceciliataboada1@gmail.com>
2023-07-27 20:54:55 -0300

    refactoring the code

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from flask import render_template
-from auth.new_user import create_user
-from auth.login_user import login
 from settings.settings_db import create_app
+from services.user_services import UserServices
 
 app = create_app()
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
     try:
-        create_user()
+        return UserServices.create_user()
     except:
-        login()
-    finally:
-        return render_template("index.html")
+        return UserServices.login()
 
