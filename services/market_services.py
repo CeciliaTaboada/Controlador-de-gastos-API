3dda27d5d8bce45d7bee7aece6144b5a4d101f85
Cecilia <ceciliataboada1@gmail.com>
2023-08-05 22:06:31 -0300

    code were fixed

--- services/market_services.py
+++ services/market_services.py
@@ -2,10 +2,11 @@ from flask import request, render_template
 from flask_login import current_user
 from models.db_models import Market
 from settings.settings_db import db
+from sqlalchemy import asc
+import pandas as pd
 
 class MarketServices():
 
-    @staticmethod
     def new_market():
         if request.method == 'POST':
             dic = {}
@@ -16,33 +17,26 @@ class MarketServices():
             for _ in range(1, lenght):
                 stuff = request.form.get("stuff")
                 price = request.form.get("price")
-                total += price
+                total += int(price)
                 dic[stuff] = price
 
-            mark = Market(market=total, market_list=dic)
+            mark = Market(market=total, market_list=dic, user_id=current_user.id)
             db.session.add(mark)
             db.session.commit()
         return render_template("home.html", user=current_user)
-    
-    @staticmethod
-    def delivery():
-        if request.method == 'POST':
-            deli = request.form.get("delivery")
-
-            new_deli = Market(delivery=deli)
-            db.session.add(new_deli)
-            db.session.commit()
-        return render_template("home.html", user=current_user)
-    
+        
     @staticmethod
     def latest_market():
-        return Market.query(Market.market).first()
+        return db.session.execute(db.select(Market.market).order_by(asc(Market.date))).first()
     
     @staticmethod
     def all_market():
-        dic = Market.query(Market.market_list).all()
-
-        for _ in dic:
+        dic = db.session.execute(db.select(Market.market_list))
+        date = db.session.execute(db.select(Market.date))
+        data_m = []
+        date_m = []
+        for _ in date:
             for key, value in dic.items():
-                """ create a function with a query to de dic  """
-        pass
+                data_m.append({key: value})
+            date_m.append(data_m)
+        return pd.DataFrame(date_m)
